a370234317d08eb7aa00ffb8e68d53bb701fd785
abl3l <45847298+abl3l@users.noreply.github.com>
2019-10-09 18:35:19 +0300

    hm11

--- js/hm11/webpack.config.js
+++ js/hm11/webpack.config.js
@@ -12,8 +12,7 @@ module.exports = ({ mode = 'production' }) =>
       mode,
       entry: './src/index.js',
       output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, "dist")
       },
       module: {
         rules: [
